b9776b53d463a5510eefff3d1f7d7ac25934c173
Adam Shannon <adamkshannon@gmail.com>
2017-01-06 11:02:14 -0600

    cmd/import: Basic reading of json from disk

--- cmd/import.go
+++ cmd/import.go
@@ -1,10 +1,45 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func Import(path, file string) error {
-	fmt.Printf("Import - %s", path)
+	abs, err := filepath.Abs(file)
+	if err != nil {
+		return err
+	}
+
+	// Check the input file exists
+	if _, err := os.Stat(abs); err != nil {
+		f, err := os.Create(abs)
+		defer f.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Read input file
+	b, err := ioutil.ReadFile(abs)
+	if err != nil {
+		return err
+	}
+
+	// Parse data
+	var wrap Wrap
+	err = json.Unmarshal(b, &wrap)
+	if err != nil {
+		return err
+	}
+
+	// Write each keypair to vault
+	for _, pair := range wrap.Data {
+		fmt.Printf("%s - %d\n", pair.Key, len(pair.Value))
+	}
+
 	return nil
 }
