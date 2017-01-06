6131995949e2437e9153a5c15eaf65d3139cc898
Adam Shannon <adamkshannon@gmail.com>
2017-01-06 11:15:37 -0600

    cmd: Note for myself

--- cmd/json.go
+++ cmd/json.go
@@ -1,5 +1,7 @@
 package cmd
 
+// todo: Store the vault address to help warn on export/import against the same vault
+
 // Json formatting for export and import
 type Wrap struct {
 	Data []Pair `json:"data"`
